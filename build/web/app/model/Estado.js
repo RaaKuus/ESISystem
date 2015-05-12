656a3255eb2d22989fabb7523bd6a9a49b34454e
RaaKuus <bruno_santos_sousa@outlook.com>
2015-05-12 18:50:38 -0300

    att

--- build/web/app/model/Estado.js
+++
@@ -1,9 +0,0 @@
-Ext.define("ESISystem.model.Estado", {
-	extend: "Ext.data.Model",
-	
-	fields: [{name: 'sigla', type: 'string'},
-	         {name: 'nome', type: 'string'},
-	         {name: 'cidades'}],
-	
-	
-});
--- build/web/app/view/MainPanel.js
+++
@@ -1,6 +0,0 @@
-Ext.define("ESISystem.view.MainPanel", {
-    extend: 'Ext.tab.Panel',
-    xtype: 'mainpanel',
-    region: 'center'
-});
-
